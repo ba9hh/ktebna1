c82303d7749c23cd18d6ad85db224521a4fd41c8
ba9hh <zedtourheart@gmail.com>
2026-04-18 18:15:46 +0100

    first commit

--- src/account/UserPosts.jsx
+++ src/account/UserPosts.jsx
@@ -121,9 +121,7 @@ const UserPosts = () => {
         <Plus className="w-5 h-5 sm:hidden" />
         <span className="hidden sm:inline">{t("userPosts.addPost")}</span>
       </button>
-      <h2 className="text-base font-semibold mb-4">
-        {t("userPosts.yourPosts")}
-      </h2>
+      <h2 className="text-lg font-semibold mb-4">{t("userPosts.yourPosts")}</h2>
       <hr />
 
       {isLoading ? (
